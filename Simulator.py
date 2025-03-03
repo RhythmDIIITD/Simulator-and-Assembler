88a016507eae31d701f6bdc3994d12dcb010d347
Rhythm Dahiya <rhythm24457@iiitd.ac.in>
2025-03-03 15:29:23 +0530

    Basic Structure for Simulator

--- Simulator.py
+++ Simulator.py
@@ -1,1 +1,257 @@
-print("Hello world")
+
+def readfile(file_path):
+    list = []
+    with open(file_path, "r") as file:
+        for line in file:
+            list.append(line.strip())
+    
+    return list
+
+Binary_Address_to_CommonName_Dic = {
+    "00000" : "x0",
+    "00001" : "x1",
+    "00010" : "x2",
+    "00011" : "x3",
+    "00100" : "x4",
+    "00101" : "x5",
+    "00110" : "x6",
+    "00111" : "x7",
+    "01000" : "x8",
+    "01001" : "x9",
+    "01010" : "x10",
+    "01011" : "x11",
+    "01100" : "x12",
+    "01101" : "x13",
+    "01110" : "x14",
+    "01111" : "x15",
+    "10000" : "x16",
+    "10001" : "x17",
+    "10010" : "x18",
+    "10011" : "x19",
+    "10100" : "x20",
+    "10101" : "x21",
+    "10110" : "x22",
+    "10111" : "x23",
+    "11000" : "x24",
+    "11001" : "x25",
+    "11010" : "x26",
+    "11011" : "x27",
+    "11100" : "x28",
+    "11101" : "x29",
+    "11110" : "x30",
+    "11111" : "x31"}
+
+Register_Value_Dictionary = {
+    "x0" : "0",
+    "x1" : "0",
+    "x2" : "0",
+    "x3" : "0",
+    "x4" : "0",
+    "x5" : "0",
+    "x6" : "0",
+    "x7" : "0",
+    "x8" : "0",
+    "x9" : "0",
+    "x10" : "0",
+    "x11" : "0",
+    "x12" : "0",
+    "x13" : "0",
+    "x14" : "0",
+    "x15" : "0",
+    "x16" : "0",
+    "x17" : "0",
+    "x18" : "0",
+    "x19" : "0",
+    "x20" : "0",
+    "x21" : "0",
+    "x22" : "0",
+    "x23" : "0",
+    "x24" : "0",
+    "x25" : "0",
+    "x26" : "0",
+    "x27" : "0",
+    "x28" : "0",
+    "x29" : "0",
+    "x30" : "0",
+    "x31" : "0"
+}
+
+Data_Memory_Dictionary = {
+    "0x00010000" : "0",
+    "0x00010004" : "0",
+    "0x00010008" : "0",
+    "0x0001000C" : "0",
+    "0x00010010" : "0",
+    "0x00010014" : "0",
+    "0x00010018" : "0",
+    "0x0001001C" : "0",
+    "0x00010020" : "0",
+    "0x00010024" : "0",
+    "0x00010028" : "0",
+    "0x0001002C" : "0",
+    "0x00010030" : "0",
+    "0x00010034" : "0",
+    "0x00010038" : "0",
+    "0x0001003C" : "0",
+    "0x00010040" : "0",
+    "0x00010044" : "0",
+    "0x00010048" : "0",
+    "0x0001004C" : "0",
+    "0x00010050" : "0",
+    "0x00010054" : "0",
+    "0x00010058" : "0",
+    "0x0001005C" : "0",
+    "0x00010060" : "0",
+    "0x00010064" : "0",
+    "0x00010068" : "0",
+    "0x0001006C" : "0",
+    "0x00010070" : "0",
+    "0x00010074" : "0",
+    "0x00010078" : "0",
+    "0x0001007C" : "0"
+}
+
+def Identify_Intrusction_Dictionary(string):
+    func3 = string[17:20]
+    opcode = string[25:32]
+
+    if func3 == "000" and opcode == "0110011":
+        if string[31:25] == "0000000":
+            return "add"
+        elif string[31:25] == "0100000":
+            return "sub"
+    elif func3 == "010" and opcode == "0110011":
+        return "slt"
+    elif func3 == "101" and opcode == "0110011":
+        return "srl"
+    elif func3 == "110" and opcode == "0110011":
+        return "or"
+    elif func3 == "111" and opcode == "0110011":
+        return "and"
+    elif func3 == "000" and opcode == "0010011":
+        return "addi"
+    elif func3 == "010" and opcode == "0000011":
+        return "lw"
+    elif func3 == "000" and opcode == "1100111":
+        return "jalr"
+    elif func3 == "010" and opcode == "0100011":
+        return "sw"
+    elif func3 == "000" and opcode == "1100011":
+        return "beq"
+    elif func3 == "001" and opcode == "1100011":
+        return "bne"
+    elif func3 == "100" and opcode == "1100011":
+        return "blt"
+    elif opcode == "1101111":
+        return "jal"
+    else:
+        return "error"
+    
+def instructioncreation(list):
+    Instructionrecognised = []
+    for instruction in list:
+        Instructionrecognised.append(Identify_Intrusction_Dictionary(instruction))
+    return Instructionrecognised
+    
+instruction_list = readfile("simple_1.txt")
+print(instruction_list)
+
+Written_Instruction_List = instructioncreation(instruction_list)
+print(Written_Instruction_List)
+
+
+def Instruction_Executor(listbinary, listwritten):
+    for i in range(len(listbinary)):
+        if listwritten[i] == "add":
+            add(listbinary[i])
+        elif listwritten[i] == "sub":
+            sub(listbinary[i])
+        elif listwritten[i] == "slt":
+            slt(listbinary[i])
+        elif listwritten[i] == "srl":
+            srl(listbinary[i])
+        elif listwritten[i] == "or":
+            orfunction(listbinary[i])
+        elif listwritten[i] == "and":
+            and_(listbinary[i])
+        elif listwritten[i] == "addi":
+            addi(listbinary[i])
+        elif listwritten[i] == "lw":
+            lw(listbinary[i])
+        elif listwritten[i] == "jalr":
+            jalr(listbinary[i])
+        elif listwritten[i] == "sw":
+            sw(listbinary[i])
+        elif listwritten[i] == "beq":
+            beq(listbinary[i])
+        elif listwritten[i] == "bne":
+            bne(listbinary[i])
+        elif listwritten[i] == "blt":
+            blt(listbinary[i])
+        elif listwritten[i] == "jal":
+            jal(listbinary[i])
+        else:
+            errorinstructionnotfound()
+
+
+def add(string):
+
+    pass
+
+def sub(string):
+
+    pass
+
+def slt(string):
+
+    pass
+
+def srl(string):
+
+    pass
+
+def orfunction(string):
+
+    pass
+
+def and_(string):
+
+    pass
+
+def addi(string):
+
+    pass
+
+def lw(string):
+
+    pass
+
+def jalr(string):
+
+    pass
+
+def sw(string):
+
+    pass
+
+def beq(string):
+
+    pass
+
+def bne(string):
+
+    pass
+
+def blt(string):
+
+    pass
+
+def jal(string):
+
+    pass
+
+def errorinstructionnotfound():
+
+    print("Error: Instruction not found")
+
+
